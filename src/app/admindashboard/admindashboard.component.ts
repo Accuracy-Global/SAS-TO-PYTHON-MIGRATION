e5d6518140cda2668a09b01c1ed0b6c6fe85bfd3
AnjaliAccuracy <anjali@accuracy.com.sg>
2023-04-28 16:44:09 +0530

    static graph on dashboard

--- src/app/admindashboard/admindashboard.component.ts
+++ src/app/admindashboard/admindashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-admindashboard',
@@ -8,8 +9,102 @@ import { Component, OnInit } from '@angular/core';
 export class AdmindashboardComponent implements OnInit {
 
   constructor() { }
+  @ViewChild('mychart') mychart;
+  canvas: any;
+  ctx: any;
+  public chart: Chart;
 
+  ngAfterViewInit() {
+    this.canvas = this.mychart.nativeElement; 
+    this.ctx = this.canvas.getContext('2d');
+
+    let myChart = new Chart(this.ctx, {
+      type: 'line',
+      
+      data: {
+        datasets: [{
+          label: 'Week',
+          backgroundColor: "rgba(255, 99, 132,0.4)",
+          borderColor: "rgb(255, 99, 132)",
+          fill: true,
+          data: [
+            { x: 1, y: 2 },
+            { x: 2, y: 2.5 },
+            { x: 3, y: 5 },
+            { x: 4, y: 4.75 },
+            { x: 5, y: 4.75 },
+            { x: 6, y: 6 },
+            { x: 7, y: 9 },
+            { x: 8, y: 6 },
+          ],
+        }]
+      },
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Sheduler Chart'
+        },
+        scales: {
+          xAxes: [{
+            type: 'linear',
+            position: 'bottom',
+            ticks: {
+              userCallback: function (tick) {
+                // if (tick >= 1000) {
+                //   return (tick / 1000).toString() + 'km';
+                // }
+                return tick.toString() + ' Week';
+              }
+            },
+            scaleLabel: {
+              labelString: 'Weeks',
+              display: true,
+            }
+          }],
+          yAxes: [{
+            type: 'linear',
+            ticks: {
+              userCallback: function (tick) {
+                return tick.toString() + 'm';
+              }
+            },
+            scaleLabel: {
+              labelString: 'Task',
+              display: true
+            }
+          }]
+        }
+      }
+    });
+  }
   ngOnInit(): void {
+    this.chart = new Chart("canvas", {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: "rgba(255, 99, 132,0.4)",
+            borderColor: "rgb(255, 99, 132)",
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
   }
 
 }
